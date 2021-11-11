296efb84065027dc2fbbd3a634b2430e492dc68e
Xelow <XELOXLEGRANDE@gmail.com>
2021-11-11 15:59:57 +0000

    quelque tests

--- src/main/java/fr/vx/rpg/classes/Items.java
+++ src/main/java/fr/vx/rpg/classes/Items.java
@@ -1,16 +1,34 @@
 package fr.vx.rpg.classes;
 
 import org.bukkit.Material;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Items {
 	
-	public static DroppedItem DIAMOND;
-	public static CraftedItem TEST;
+	public static DroppedItem AMETHYSTE_SHARD;
+	public static DroppedItem CONDENSED_AMETHYSTE;
+	public static DroppedItem GOLD_NUGGET;
+
+	public static CraftedItem MAGIC_UPGRADE;
 	
 	public static void register() {
-		
-		DIAMOND = new DroppedItem(Material.DIAMOND, "§bDiamant", Rarity.VERY_RARE, 10000, Material.STONE, 100);
-		
+
+		AMETHYSTE_SHARD = new DroppedItem(Material.PURPLE_DYE, "§fEclat d'Améthyste", Rarity.VERY_COMMON, 400, Material.PURPLE_STAINED_GLASS, 16);
+		CONDENSED_AMETHYSTE = new DroppedItem(Material.PURPLE_STAINED_GLASS, "§bAméthyste condensée", Rarity.RARE, 3600, Material.PURPLE_WOOL, 64);
+		GOLD_NUGGET = new DroppedItem(Material.GOLD_NUGGET, "§2Pepite d'or", Rarity.UNCOMMON, 100, Material.GOLD_ORE, 12);
+
+		List MAGIC_UPGRADE_INGREDIENTS = new LinkedList();
+		MAGIC_UPGRADE_INGREDIENTS.add(GOLD_NUGGET.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(AMETHYSTE_SHARD.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(GOLD_NUGGET.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(AMETHYSTE_SHARD.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(CONDENSED_AMETHYSTE.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(AMETHYSTE_SHARD.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(GOLD_NUGGET.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(AMETHYSTE_SHARD.getItemStack());
+		MAGIC_UPGRADE_INGREDIENTS.add(GOLD_NUGGET.getItemStack());
+		MAGIC_UPGRADE = new CraftedItem(Material.CREEPER_BANNER_PATTERN, "§1Amelioration magique", Rarity.VERY_RARE, 7600, MAGIC_UPGRADE_INGREDIENTS, 1);
 	}
 
 }
