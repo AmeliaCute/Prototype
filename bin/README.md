2599b5ba69015d8f21ffd448ac57753857c118a2
Venodez <30352734+Venodez@users.noreply.github.com>
2021-11-26 00:45:14 +0100

    Add NMS Lib

---
+++ bin/README.md
@@ -0,0 +1,6 @@
+# Prototype
+
+## House map :
+
+##### [City](https://cdn.discordapp.com/attachments/538082225099636737/913513650390695936/New_World.rar)
+##### [House interior](https://cdn.discordapp.com/attachments/538082225099636737/913518378315038780/maison-interieur.rar)
---
+++ src/main/java/fr/vx/rpg/classes/mobs/Mob.java
@@ -0,0 +1,15 @@
+package fr.vx.rpg.classes.mobs;
+
+import net.minecraft.server.v1_16_R3.EntityCreature;
+import net.minecraft.server.v1_16_R3.EntityTypes;
+import net.minecraft.server.v1_16_R3.World;
+
+public class Mob extends EntityCreature {
+
+	protected Mob(EntityTypes<? extends EntityCreature> entitytypes, World world) {
+		super(entitytypes, world);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
